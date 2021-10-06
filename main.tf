c86093f6fdf885922eb5bfa96add4c99197f5a94
sameera-saleem <71988780+sameera-saleem@users.noreply.github.com>
2021-10-06 15:51:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,10 @@ variable "instance_name"{
   default = " "
 }
 
-resource "aws_key_pair" "terraform-demo" {
-  key_name   = "terraform-demo"
-  public_key = "${file("~/.ssh/id_rsa.pub")}"
-}
-
 resource "aws_instance" "my-instance" {
   count         = 1
   ami           = "ami-5b41123e"
   instance_type = var.instance_type
-  key_name      = "${aws_key_pair.terraform-demo.key_name}"
 
   tags = {
     Name  = var.instance_name
