46f5935e08c08457eec1709faddbe7b5633b5954
sameera-saleem <71988780+sameera-saleem@users.noreply.github.com>
2021-10-06 14:30:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,19 @@ provider {
   region = "us-east-1"
 }
 
-resource "aws_instance" "vm-one" {
+resource "aws_key_pair" "terraform-demo" {
+  key_name   = "terraform-demo"
+  public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_instance" "my-instance" {
+  count         = 1
   ami           = "ami-5b41123e"
-  instance_type = "t2.micro"
-  
+  instance_type = var.instance_type
+  key_name      = "${aws_key_pair.terraform-demo.key_name}"
+
   tags = {
-    Name = "VM-one"
+    Name  = "sample_one"
   }
 }
+
